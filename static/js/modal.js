1a16267d1bdcb5693c2ce5f0d9df8b24ab5df404
mohsenjfar <mohsen.j.far@gmail.com>
2026-06-07 17:35:31 +0330

    update

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,11 +1,14 @@
 const modal = document.getElementById("toolModal")
 const modalBody = document.getElementById("modalBody")
+const modalTitle = document.getElementById("modalTitle")
 
 function openTool(tool){
 
 modal.style.display = "flex"
 
-modalBody.innerHTML = tools[tool]
+modalTitle.innerText = tools[tool].title
+
+modalBody.innerHTML = tools[tool].html
 
 }
 
