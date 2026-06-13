96ddff26aebce3a0c1a2f854f96f1b3c79a1cbb9
mohsenjfar <mohsen.j.far@gmail.com>
2026-06-13 19:04:31 +0330

    map ref

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,26 +1,24 @@
 /* file_path: /static/js/modal.js */
 
-const modal = document.getElementById("toolModal")
-const modalBody = document.getElementById("modalBody")
-const modalTitle = document.getElementById("modalTitle")
+const modal = document.getElementById("toolModal");
+const modalBody = document.getElementById("modalBody");
+const modalTitle = document.getElementById("modalTitle");
 
 function openTool(tool) {
-    // اگر ابزار map است، به صفحه جداگانه هدایت شود
-    if (tool === 'map') {
-        window.location.href = '/map';
-        return;
-    }
-    
-    modal.style.display = "flex"
-    modalTitle.innerText = tools[tool].title
-    modalBody.innerHTML = tools[tool].html
-    initToolLogic(tool)
+  // اگر ابزار map است، به صفحه جداگانه هدایت شود
+  if (tool === "map") {
+    window.location.href = "/map";
+    return;
+  }
+
+  modal.style.display = "flex";
+  modalTitle.innerText = tools[tool].title;
+  modalBody.innerHTML = tools[tool].html;
+  initToolLogic(tool);
 }
 
-function closeModal(){
+function closeModal() {
+  modal.style.display = "none";
 
-modal.style.display = "none"
-
-modalBody.innerHTML = ""
-
+  modalBody.innerHTML = "";
 }
