44db1d35b1991d59c45d41088b34eda2b1c45f4a
Mohsen <mohsen.j.far@gmail.com>
2026-07-04 05:15:26 +0000

    add region baharesan

--- helpers/.ipynb_checkpoints/register_kernel-checkpoint.sh
+++ helpers/.ipynb_checkpoints/register_kernel-checkpoint.sh
@@ -1,1 +1,3 @@
 python -m ipykernel install --user --name=sadrafars --display-name="Python (sadrafars)"
+mv baharestan.geojson ../static/data/districts/
+git add . && git commit -m "add region " && git push
